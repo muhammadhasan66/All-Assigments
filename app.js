4c07dfa8dc82c2734f9f581e58fb1a6fd4e8b1a1
muhammad hasan <muhammadhasan3866@gmail.com>
2023-02-27 11:32:50 +0500

    Copy and paste

---
+++ app.js
@@ -0,0 +1,145 @@
+// ------------Question no 1------------
+
+
+// let assign = [];
+
+
+// ------------Question no 3------------
+
+// let strings = ["Muhammad Hammad", "Ali", "Muhammad Hassan", "Murfad"];
+
+
+
+// ------------Question no 4------------
+// let numbers = [1, 2, 3, 4, 5];
+
+
+// ------------Question no 5------------
+// let boolean = [true, false];
+
+
+
+// ------------Question no 6------------
+// let mixedArray = ["string", 12, varaible, true];
+
+
+
+
+
+
+// ------------Question no 7------------
+// let qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+// document.write("<h1>Qualifications</h1> <br>");
+// document.write(`1) ${qualifications[0]} <br>`);
+// document.write(`2) ${qualifications[1]} <br>`);
+// document.write(`3) ${qualifications[2]} <br>`);
+// document.write(`4) ${qualifications[3]} <br>`);
+// document.write(`5) ${qualifications[4]} <br>`);
+// document.write(`6) ${qualifications[5]} <br>`);
+// document.write(`7) ${qualifications[6]} <br>`);
+// document.write(`8) ${qualifications[7]} <br>`);
+
+
+
+// ------------Question no 8------------
+let students = ["Micheal", "John", "Tony"];
+let studentsScore = [320, 230, 480];
+document.write(`Score of ${students[0]} is ${studentsScore[0]}. Percentage: ${studentsScore[0] / 500 * 100}% <br>`);
+document.write(`Score of ${students[1]} is ${studentsScore[1]}. 
+Percentage: ${studentsScore[1] / 500 * 100}% <br>`);
+document.write(`Score of ${students[2]} is ${studentsScore[2]}. Percentage: ${studentsScore[2] / 500 * 100}% <br> ,<br>,<br>`);
+
+
+// ------------Question no 9------------
+const colors = ["Red", "Green", "Blue", "Yellow", "Purple"];
+document.write(`${colors} <br>`);
+
+
+// ------------Section "A"------------
+// const askBiginColor = prompt("What color do you wants to add to the beginning Array");
+colors.unshift("orange");
+document.write(`${colors} <br>`);
+
+
+// ------------Section "B"------------
+// const askEndColor = prompt("What color do you wants to add to the Ending Array");
+// colors.push(askEndColor);
+// document.write(`${colors} <br>`);
+
+
+// ------------Section "C"------------
+// colors.unshift("Purple", "Orange");
+// document.write(`${colors} <br>`);
+
+
+// ------------Section "D"------------
+// colors.shift();
+// document.write(`${colors} <br>`);
+
+
+// ------------Section "E"------------
+// colors.pop();
+// document.write(`${colors} <br>`);
+
+
+// ------------Section "F"------------
+// const askMidColorIndex = +prompt("What Index number do you wants to add to the Middle Array");
+// const askMidColorName = prompt("What color name do you wants to add to the Middle Array");
+// colors.splice(askMidColorIndex, 0, askMidColorName);
+// document.write(`${colors} <br>`);
+
+
+// ------------Section "g"------------
+// const askMidDelColorIndex = +prompt("What Index number do you wants start to delete at this Array");
+// const addMidDelColorNo = +prompt("What color names do you wants to delete to the Array");
+// colors.splice(askMidDelColorIndex, addMidDelColorNo);
+// document.write(`${colors} <br>`);
+
+
+
+
+// ------------Question no 10------------
+
+// const studentsScores = [320, 230, 480, 120];
+// document.write(`Scores of Students : ${studentsScores} <br>`);
+// const studentsScoresSort = studentsScores.sort();
+// document.write(`Ordered Scores of Students : ${studentsScoresSort} <br>`);
+
+
+
+// ------------Question no 11------------
+const fruits = ["strawberry", "apple", "orange", "banana"];
+document.write(`<h1>Fruits List:</h1> ${fruits}`);
+ fruits.sort();
+document.write(`<h1>Ordered fruits List:</h1> ${fruits}`);
+
+
+
+
+
+// ------------Question no 12------------
+
+
+
+// const cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+// document.write(`<h1>Cities List:</h1> ${cities}`);
+// const selectedCities = cities.slice(2 , 4);
+// document.write(`<h1>Selected cities List:</h1> ${selectedCities}`);
+
+
+// ------------Questiozn no 13------------
+// const devices = ["keyboard", "mouse", "printer", "moniter"];
+// document.write(`<h3>Devices:</h3> ${devices}`);
+// document.write(`<h4>Out:</h4> ${devices[0]}`);
+// document.write(`<h4>Out:</h4> ${devices[1]}`);
+// document.write(`<h4>Out:</h4> ${devices[2]}`);
+// document.write(`<h4>Out:</h4> ${devices[3]}`);
+
+
+// ------------Question no 14------------
+// const devices = ["keyboard", "mouse", "printer", "moniter"];
+// document.write(`<h3>Devices:</h3> ${devices}`);
+// document.write(`<h4>Out:</h4> ${devices[3]}`);
+// document.write(`<h4>Out:</h4> ${devices[2]}`);
+// document.write(`<h4>Out:</h4> ${devices[1]}`);
+// document.write(`<h4>Out:</h4> ${devices[0]}`);
